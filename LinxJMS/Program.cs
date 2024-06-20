08bab5b7086ed63f074b7ab88cdd8e395cf28e72
Bernhard Häussermann <bernhard.haeussermann@digiata.com>
2024-06-20 13:17:27 +0200

    Add existing project

---
+++ LinxJMS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LinxJMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var matchSend = args.FirstOrDefault(stringToCheck => stringToCheck.Contains("-send"));
+
+            if (matchSend != null)
+            {
+              
+                new csMsgProducer(args);
+            }
+            var matchRead = args.FirstOrDefault(stringToCheck => stringToCheck.Contains("-read"));
+
+            if (matchRead != null)
+            {
+             
+                new csMsgConsumer(args);
+            }
+
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
